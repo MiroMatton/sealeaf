12dbedf0a2df5c35a6f3432920580d5d6b01d3b6
miromatton <miro.matton@student.odisee.be>
2021-10-14 00:15:42 +0200

    instagram function added

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ const navLink = document.querySelectorAll(".nav-link");
 const body = document.getElementsByTagName("BODY")[0];
 
 hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    body.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  body.classList.toggle("active");
 }
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    body.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  body.classList.remove("active");
 }
